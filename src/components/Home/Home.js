305041259b3a2e9c99f7e9e614d07fe0776d1758
Matias Salicru <mattiassalicru@gmail.com>
2020-10-26 15:26:04 -0300

    First commit of the proyect

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '../Nav/Navbar'
+
+export const Home = () => {
+    return (
+        <div className='home-container'>
+
+            <Navbar/>
+
+        </div>
+    )
+}
---
+++ src/components/Nav/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css';
+
+export const Navbar = () => {
+    return (
+        <nav className='nav-container'>
+            <img src="" alt="logo"/>
+
+            <div>
+                
+            </div>
+        </nav>
+    )
+}
